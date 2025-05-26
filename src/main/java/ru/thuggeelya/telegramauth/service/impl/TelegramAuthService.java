eb6f97c0bebb067a0c27f88e33f67b75ba6ee8aa
thuggeelya <ilya-fdmdh@mail.ru>
2025-05-27 01:17:28 +0300

    fix: Обновление пользователя

--- src/main/java/ru/thuggeelya/telegramauth/service/impl/TelegramAuthService.java
+++ src/main/java/ru/thuggeelya/telegramauth/service/impl/TelegramAuthService.java
@@ -2,6 +2,7 @@ package ru.thuggeelya.telegramauth.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.thuggeelya.telegramauth.dao.entity.User;
 import ru.thuggeelya.telegramauth.dto.UserDto;
 import ru.thuggeelya.telegramauth.service.AuthService;
@@ -14,18 +15,29 @@ public class TelegramAuthService implements AuthService {
     private final UserService userService;
 
     @Override
+    @Transactional
     public User login(final UserDto dto) {
 
-        return userService.findById(dto.getId()).orElseGet(
-                () -> userService.save(
-                        User.builder()
-                            .id(dto.getId())
-                            .firstName(dto.getFirstName())
-                            .lastName(dto.getLastName())
-                            .username(dto.getUsername())
-                            .photoUrl(dto.getPhotoUrl())
-                            .build()
-                )
-        );
+        final User user = userService.findById(dto.getId())
+                                     .map(existing -> {
+
+                                         existing.setFirstName(dto.getFirstName());
+                                         existing.setLastName(dto.getLastName());
+                                         existing.setUsername(dto.getUsername());
+                                         existing.setPhotoUrl(dto.getPhotoUrl());
+
+                                         return existing;
+                                     })
+                                     .orElseGet(
+                                             () -> User.builder()
+                                                       .id(dto.getId())
+                                                       .firstName(dto.getFirstName())
+                                                       .lastName(dto.getLastName())
+                                                       .username(dto.getUsername())
+                                                       .photoUrl(dto.getPhotoUrl())
+                                                       .build()
+                                     );
+
+        return userService.save(user);
     }
 }
